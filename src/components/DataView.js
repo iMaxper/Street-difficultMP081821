700524f5c0a7fe0253bfb07b75285ebe9d39ce67
Maxwell <jpereira4@gmail.com>
2021-08-18 21:55:53 +0000

    formatted values and calc of pcts

--- src/components/DataView.js
+++ src/components/DataView.js
@@ -16,11 +16,11 @@ function DataView(props) {
               <h3 className="text-header">Renters</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">${props.renterCost}</h2>
+              <h2 className="text-data">${Number(props.renterCost).toLocaleString('en-US')}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">${props.renterIncome}</h2>
+              <h2 className="text-data">${Number(props.renterIncome).toLocaleString('en-US')}</h2>
               <h3 className="text-label">Annual Income</h3>
             </div>
             <div className="text bottom">
@@ -33,11 +33,11 @@ function DataView(props) {
               <h3 className="text-header">Homeowners</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">${props.ownerCost}</h2>
+              <h2 className="text-data">${Number(props.ownerCost).toLocaleString('en-US')}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">${props.ownerIncome}</h2>
+              <h2 className="text-data">${Number(props.ownerIncome).toLocaleString('en-US')}</h2>
               <h3 className="text-label">Annual Income</h3>
             </div>
             <div className="text bottom">
