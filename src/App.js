5db67319a8979923054dc2482d5d643df9a13fd2
Maxwell <jpereira4@gmail.com>
2021-08-18 19:44:58 +0000

    Added data values and percents-081821

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
       <Navbar/>
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
 
-      <DataView/>
+      <DataView borough="Staten Island" renterCost="14292" ownerCost="29752" renterIncome="37882" ownerIncome="94177" renterCostRatio="" ownerCostRatio=""/>
+      <DataView borough="Queens" renterCost="16812" ownerCost="29256" renterIncome="46549" ownerIncome="80144"renterCostRatio="" ownerCostRatio=""/>
+      <DataView borough="Brooklyn" renterCost="15144" ownerCost="31908" renterIncome="40128" ownerIncome="83737" renterCostRatio="" ownerCostRatio=""/>
+      <DataView borough="Manhattan" renterCost="18900" ownerCost="36252" renterIncome="62173" ownerIncome="142046" renterCostRatio="" ownerCostRatio=""/>
+      <DataView borough="Bronx" renterCost="13176" ownerCost="28548"renterIncome="29302" ownerIncome="76454" renterCostRatio="" ownerCostRatio="" />
     </div>
   );
 }
--- src/components/DataView.js
+++ src/components/DataView.js
@@ -16,8 +16,16 @@ function DataView(props) {
               <h3 className="text-header">Renters</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">{props.renterCost}</h2>
+              <h2 className="text-data">${props.renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
+            </div>
+            <div className="text">
+              <h2 className="text-data">${props.renterIncome}</h2>
+              <h3 className="text-label">Annual Income</h3>
+            </div>
+            <div className="text bottom">
+              <h2 className="text-data">{Math.round(Number(props.renterCost)/Number(props.renterIncome)*100)}%</h2>
+              <h3 className="text-label">Cost to Income Ratio</h3>
             </div>
           </div>
           <div className="data-view-column right">
@@ -25,8 +33,16 @@ function DataView(props) {
               <h3 className="text-header">Homeowners</h3>
             </div>
             <div className="text">
-              <h2 className="text-data">{}</h2>
+              <h2 className="text-data">${props.ownerCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
+            </div>
+            <div className="text">
+              <h2 className="text-data">${props.ownerIncome}</h2>
+              <h3 className="text-label">Annual Income</h3>
+            </div>
+            <div className="text bottom">
+              <h2 className="text-data">{Math.round(Number(props.ownerCost)/Number(props.ownerIncome)*100)}%</h2>
+              <h3 className="text-label">Cost to Income Ratio</h3>
             </div>
           </div>
         </div>
